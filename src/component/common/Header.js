15c5a6ddf2f37784d23212d5dbde7862f42e6f9f
Carmelo <Carmelo_Xue@Carmelo.com>
2017-07-15 11:29:01 +0800

    webpack 3rd lib pack, bootstrap carousel

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-
 const Header = ()=>{
   return (
   <nav className="nav navbar-default navbar-fixed-top">
@@ -7,9 +6,25 @@ const Header = ()=>{
       <div className="navbar-header">
         <a href="/" className="navbar-brand">Brand</a>
       </div>
+      <ul className="nav navbar-nav">
+        <li><a>Page One</a></li>
+        <li><a>Page Two</a></li>
+      </ul>
+
+      <ul className="nav navbar-nav navbar-right">
+        <li><a>Login</a></li>
+      </ul>
+
+      <form className="navbar-form navbar-right">
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Search"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Search</button>
+      </form>
+
+
     </div>
   </nav>
   )
 }
-
 export default Header
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import {Link} from 'react-router';
 
 const HomePage = ()=>{
   return (
-    <div className="jumbotron">
+    /*<div classNameName="jumbotron">
       <h1>Folk Star</h1>
       <p>Every Folk is a shiny Star</p>
-      <Link to="about" className="btn btn-primary">Learn More</Link>
-    </div>
+      <Link to="about" classNameName="btn btn-primary">Learn More</Link>
+    </div>*/
+   <div className="container">
+     <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+
+       <ol className="carousel-indicators">
+         <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+       </ol>
+
+       <div className="carousel-inner" role="listbox">
+         <div className="item active">
+           <img src={require('../../img/kobe.jpg')} alt="..."/>
+           <div className="carousel-caption">
+             Kobe Bryant
+           </div>
+         </div>
+         <div className="item">
+           <img src={require("../../img/curry.png")} alt="..."/>
+           <div className="carousel-caption">
+             Stephan Curry
+           </div>
+         </div>
+         <div className="item">
+           <img src={require("../../img/lbj.jpg")} alt="..."/>
+           <div className="carousel-caption">
+             Lebron James
+           </div>
+         </div>
+         <div className="item">
+           <img src={require("../../img/ws.jpg")} alt="..."/>
+           <div className="carousel-caption">
+             Russell Westbrook
+           </div>
+         </div>
+       </div>
+
+       <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+         <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+         <span className="sr-only">Previous</span>
+       </a>
+       <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+         <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+         <span className="sr-only">Next</span>
+       </a>
+
+     </div>
+   </div>
+
   )
 }
 
